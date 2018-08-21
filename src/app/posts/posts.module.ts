734b1ff70729b66b89ef9dcbec9aa254faf719a9
mazecore <39755178+mazecore@users.noreply.github.com>
2018-08-21 13:45:25 -0400

    Add files via upload

---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { PostCreateComponent } from "./post-create/post-create.component";
+import { PostListComponent } from "./post-list/post-list.component";
+import { AngularMaterialModule } from "../angular-material.module";
+import { PostEditComponent } from "./post-edit/post-edit.component";
+
+@NgModule({
+  declarations: [PostCreateComponent, PostListComponent, PostEditComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    RouterModule
+  ]
+})
+export class PostsModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: "http://artport-env.gmrptv4xcu.us-east-2.elasticbeanstalk.com/api"
+};
